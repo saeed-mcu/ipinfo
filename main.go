6de998ee3e3cdc5ca0deb03710ecb7b61d0739c0
Saeed Padari <sam137115@gmail.com>
2025-04-18 15:23:36 +0330

    log info

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,10 +12,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if ipProxy := r.Header.Get("X-Forwarded-For"); ipProxy != "" {
 		ip = ipProxy
 	}
+
+	// Log the response
+	log.Printf("Client IP: %s\n", ip)
+
 	fmt.Fprintln(w, ip)
 }
 
 func main() {
+
+	port := getEnv("PORT", "8080")
+	addr := ":" + port
+	log.Printf("✅ Server starting on %s\n", addr)
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":80", nil)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func getEnv(key, fallback string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+	return val
 }
